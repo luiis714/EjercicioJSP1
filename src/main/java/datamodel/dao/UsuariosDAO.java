25791ca1da5269f0b5e76f1bfe0ee751a26068e5
Luis <luis.mangas15@gmail.com>
2021-04-29 18:07:57 +0200

    Creado metodo de compruebaLogin dentro de Servlet Login.java. Creado el formulario de login en HTML. Recupero los parametros pasados por formLogin.html a traves de doGet. Compilación y ejecución correctas.

--- src/main/java/datamodel/dao/UsuariosDAO.java
+++ src/main/java/datamodel/dao/UsuariosDAO.java
@@ -2,15 +2,22 @@ package datamodel.dao;
 
 import java.util.List;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 
 import datamodel.entities.Usuarios;
+import servlet.Login;
 
 public class UsuariosDAO {
+	
+	private static Logger logger = LogManager.getLogger(Login.class);
 	
 	/**Metodo que devuelve una lista con todos los usuarios*/
 	public static List<Usuarios> getAllUsuarios(Session session){
 		String hQuery = "FROM Usuarios";
+		
+		logger.info("Recupero todos los usuarios");
 		
 		List<Usuarios> usuarios = session.createQuery(hQuery, Usuarios.class)
 											.list();
@@ -18,6 +25,7 @@ public class UsuariosDAO {
 		return usuarios;
 	}
 	
+	/**Metodo que devuelve un usuario a través de su id pasado por parametro*/
 	public static Usuarios getUsuario(Session session, int idUsuario) {
 		String hQuery = " from Usuarios u " +
                 " where u.id = :idUsuario";
@@ -26,9 +34,12 @@ public class UsuariosDAO {
 									.setParameter("idUsuario", idUsuario)
 									.setMaxResults(1)
 									.uniqueResult();
+		
+		logger.info("Reciupero el usuario por su id: " + usuario.toString());
 		return usuario;
 	}
 	
+	/**Metodo que devuelve un usuario a través de su email pasado por parametro*/
 	public static Usuarios getUsuario(Session session, String email) {
 		String hQuery = " from Usuarios u " +
                 " where u.email = :email";
@@ -37,6 +48,8 @@ public class UsuariosDAO {
 									.setParameter("email", email)
 									.setMaxResults(1)
 									.uniqueResult();
+		
+		logger.info("Reciupero el usuario por su email: " + usuario.toString());
 		return usuario;
 	}
 }
