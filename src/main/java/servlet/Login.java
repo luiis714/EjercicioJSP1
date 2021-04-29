f299922a8c46827bf8b9ea6ec1c706e97c374392
Luis <luis.mangas15@gmail.com>
2021-04-29 18:07:57 +0200

    Creado metodo de compruebaLogin dentro de Servlet Login.java. Creado el formulario de login en HTML. Recupero los parametros pasados por formLogin.html a traves de doGet. Compilación y ejecución correctas.

--- src/main/java/servlet/Login.java
+++ src/main/java/servlet/Login.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -39,6 +41,7 @@ public class Login extends HttpServlet {
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
+		//Abro la session con la BBDD a través hibernate
 		session = HibernateUtil.getSessionFactory().openSession();
 		logger.info("Creo la sesión. Conexión con la BBDD. Desde MostrarDatos");
 	}
@@ -47,25 +50,14 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+
+		//Recupero los parametros de usuario y contraseña
 		String p_user = request.getParameter("user");
 		String p_pass = request.getParameter("password");
 		
-		Usuarios usuario = UsuariosDAO.getUsuario(session, p_user);
-		
-		
-		
-		if(usuario != null) {
-			if(usuario.getClave().equals(p_pass)) {
-				logger.info("Se ha logueado CORRECTAMENTE");
-			}
-			else {
-				logger.info("Password incorrecta");
-			}
-				logger.info(usuario.toString());
-		}
-		else {
-			logger.info("No se ha encontrado usuario");
-		}
+		logger.info("Voy a comprobar login con usuario: " + p_user + " clave: " + p_pass);
+		compruebaLogin(p_user, p_pass, out);
 	}
 
 	/**
@@ -74,6 +66,40 @@ public class Login extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	private void compruebaLogin(String p_user, String p_pass, PrintWriter out) {
+		Usuarios usuario = UsuariosDAO.getUsuario(session, p_user);
+		
+		if((usuario != null) && usuario.getClave().equals(p_pass)) {
+			//Muestro HTML de login correcto
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Login correcto</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<h1>BIENVENIDO " + usuario.getNombre() + "</h1>");
+			out.println("<h3>Se ha iniciado sesión de forma correcta</h3>");
+			out.println("</body>");
+			out.println("</html>");
+			
+			logger.info("Se ha logueado CORRECTAMENTE");
+			logger.info(usuario.toString());
+		}
+		else {
+			//Muestro HTML de login incorrecto
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Login incorrecto</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<h1>El usuario o la contraseña son incorrectos</h1>");
+			out.println("</body>");
+			out.println("</html>");
+			
+			logger.info("No se ha encontrado usuario o la contraseña es incorrecta");
+		}
+		
 	}
 
 }
